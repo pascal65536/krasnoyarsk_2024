555c6b4dbcbfccadcca22a8c2fd7b3b2cbceae41
defise <defrize.opw@gmail.com>
2024-12-04 19:10:15 +0700

    Добавлена функция mult_even_nums() для нахождения произведения чётных чисел из файла numbers.txt

--- main.py
+++ main.py
@@ -6,4 +6,16 @@ def mult_nums():
     for x in nums:
         mult *= int(x)
     return mult
-# Писать код сюда
+
+
+def mult_even_nums():
+    with open('numbers.txt', 'r', encoding='utf-8') as file:
+        content = file.read()
+        numbers = map(int, content.split())
+        even_numbers = [num for num in numbers if num % 2 == 0]
+
+        product = 1
+        for num in even_numbers:
+            product *= num
+
+        return product if even_numbers else 0
