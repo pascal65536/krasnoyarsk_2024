3a59a9bd9f512c62836b033ada4b7ed0673c7123
pascal65536 <pascal65536@gmail.com>
2024-12-04 10:16:45 +0700

    23

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
 for i in range(500):
-    if not i % 13:
+    if not i % 23:
         continue
-    if not i % 17:
+    if not i % 29:
         continue 
-    if not i % 19:
+    if not i % 37:
         continue       
     print(i, end=' ')
 print()
