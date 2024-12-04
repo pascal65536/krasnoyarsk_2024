4c40dbc8f235bd80f2578531bdff2f28fefe42ab
roma20ix <non@yandex.ru>
2024-12-04 16:31:21 +0700

    Написал функцию для создания файла с простыми числами

--- 1.py
+++ 1.py
@@ -1,8 +1,22 @@
-primelist = [11, 13, 17, 19, 23, 27]
-for i in range(500):
-    for p in primelist:
-        if not i % p:
-            continue   
-    print(i, end=' ')
-print()
+def create() -> None:
+    """
+    Функция создания файла с простыми числами до 500 (Все числа записаны через пробел)
+    Args:
+        None
+    Retuns:
+        None
+    """
+    with open("numbers.txt", "w") as file:
+        prime_numbers = []
+        mini = [0] * 501
+        for i in range(2, 501):
+            if mini[i] == 0:
+                mini[i] = i
+                prime_numbers.append(i)
+            for y in prime_numbers:
+                if y * i > 500 or mini[i] < y:
+                    break
+                mini[y * i] = y
 
+        prime_numbers = list(map(str, prime_numbers))
+        file.write(" ".join(list(map(str, prime_numbers))))
