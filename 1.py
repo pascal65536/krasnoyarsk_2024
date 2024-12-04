8eef1f6ff82f7317b92db03cba590a1340ccab93
pascal65536 <pascal65536@gmail.com>
2024-12-04 10:04:52 +0700

    1

--- 1.py
+++ 1.py
@@ -1,2 +1,10 @@
-import behoof
+for i in range(500):
+    if not i % 13:
+        continue
+    if not i % 17:
+        continue 
+    if not i % 19:
+        continue       
+    print(i, end=' ')
+print()
 
