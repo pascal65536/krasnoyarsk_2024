1ce3ff603358de627ba84b003a1938e286509775
Aradiant <cppr692@gmail.com>
2024-12-04 16:48:59 +0700

    removed debug

--- 1.py
+++ 1.py
@@ -1,13 +1,11 @@
-def largest(debug=False):
+def largest():
     '''
     Функция для нахождения наибольшего числа из numbers.txt.
     Args:
-        debug=False: boolean; Режим отладки
+        None
     Returns:
         int
     '''
-    with open(['', '_test_'][debug] + 'numbers.txt', 'r', encoding='utf-8') as f:
+    with open('numbers.txt', 'r', encoding='utf-8') as f:
         data = map(int, f.read().split())
-        if debug:
-            print(data)
         return max(data)
