2a805802001c3440b7c9deb6d1414b518eaadb10
pascal65536 <pascal65536@gmail.com>
2024-12-04 10:27:49 +0700

    to list

--- 1.py
+++ 1.py
@@ -1,16 +1,8 @@
+primelist = [11, 13, 17, 19, 23, 27]
 for i in range(500):
-    if not i % 11:
-        continue
-    if not i % 13:
-        continue 
-    if not i % 17:
-        continue     
-    if not i % 19:
-        continue
-    if not i % 23:
-        continue 
-    if not i % 27:
-        continue       
+    for p in primelist:
+        if not i % p:
+            continue   
     print(i, end=' ')
 print()
 
