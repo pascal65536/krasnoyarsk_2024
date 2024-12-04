6da45de67ca3a93d8d89d6ac331219f9bb003267
cloud29s <fedor151207@mail.ru>
2024-12-04 18:47:12 +0700

    я сделяль

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+def mult_nums():
+    with open('numbers.txt', encoding='utf-8') as f:
+        data = f.read()
+    nums = data.split()
+    mult = 1
+    for x in nums:
+        mult *= int(x)
+    return mult
 # Писать код сюда
