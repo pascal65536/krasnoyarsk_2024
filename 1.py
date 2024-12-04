88fdbb919a98077847d8270959cb1dfa60e18c85
Razoom1 <sergey04351@gmail.com>
2024-12-04 17:01:50 +0700

    Added maxsqr()

--- 1.py
+++ 1.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 primelist = [11, 13, 17, 19, 23, 27]
 for i in range(500):
     for p in primelist:
@@ -6,3 +9,13 @@ for i in range(500):
     print(i, end=' ')
 print()
 
+def maxsqr():
+    f = open('numbers.txt', encoding='utf-8')
+    data = f.readlines()
+    maxNum = 0
+    for j in data:
+        j = j.replace('\n', '')
+        number = sqrt(int(j))
+        if number.is_integer() and maxNum < int(j):
+            maxNum = int(j)
+    return maxNum
