1bcc5435b9c5b020c90d44bb9591030897c4bd28
Zeephty <t.flogi.ho@gmail.com>
2024-12-04 19:13:01 +0700

    1 commit

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Писать код сюда
-
-
-if __name__ == '__main__':
-    pass
+def summator():
+    with open("numbers.txt", "r", encoding="utf8") as file:
+        return sum(map(int, file.readline().split()))
