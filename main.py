1d2e702bab8b1f58b1f13aba332e119371c66d54
dobrishk <catplay4373@gmail.com>
2024-12-04 19:02:32 +0700

    add rand

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # Писать код сюда
 
+from random import randint
+
+def rand_nums():
+    with open('numbers.txt', 'w') as f:
+        for _ in range(1000):
+            f.write(str(randint(1, 1000)) + ' ')
+
 
 if __name__ == '__main__':
     pass
